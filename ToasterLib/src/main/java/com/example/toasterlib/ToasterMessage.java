a1c6858dd11ed4819c522586aa1c3e8502e2d6f4
unknown <gunjan.shakya3@gmail.com>
2021-08-04 16:27:35 +0545

    commit

--- ToasterLib/src/main/java/com/example/toasterlib/ToasterMessage.java
+++ ToasterLib/src/main/java/com/example/toasterlib/ToasterMessage.java
@@ -4,7 +4,7 @@ import android.content.Context;
 import android.widget.Toast;
 
 public class ToasterMessage {
-    public static void main(Context context, String message) {
+    public static void s(Context context, String message) {
         Toast.makeText(context, message, Toast.LENGTH_LONG).show();
     }
 }
